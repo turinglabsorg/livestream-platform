54c53f1fc0f5b4f83c4ae73f5b1bad786a43b65f
turinglabsorg <s.cataudo@turinglabs.org>
2021-01-13 16:33:29 +0100

    install script

---
+++ api/install.sh
@@ -0,0 +1,27 @@
+#INSTALL MONGODB
+wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+mkdir data
+ulimit -n 640000
+
+#DOWNLOADING NODE MODULES
+npm install
+cp example.env .env
+npm run build
+
+#UPDATING NPM
+npm install -g npm
+npm install -g pm2
+pm2 install pm2-logrotate
+pm2 set pm2-logrotate:max_size 100M
+pm2 set pm2-logrotate:compress true
+pm2 set pm2-logrotate:rotateInterval '0 * * * *'
+
+#SETTING UP FIREWALL
+ufw allow 22
+ufw deny 42223
+ufw deny 27017
+ufw allow 42222
+ufw enable y
